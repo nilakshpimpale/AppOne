d3fc961d1f8939bf534eb0e12c097868550137b6
Nilaksh <nilaksh.pimpale@gmail.com>
2026-05-23 11:55:47 +0530

    Initial Commit

---
+++ AppOne/AppOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppOneApp.swift
+//  AppOne
+//
+//  Created by Nilaksh Pimpale on 23/05/26.
+//
+
+import SwiftUI
+
+@main
+struct AppOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppOne/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppOne
+//
+//  Created by Nilaksh Pimpale on 23/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
